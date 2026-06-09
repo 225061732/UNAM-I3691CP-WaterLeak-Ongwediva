e7a92ebf4e830f80b986db47e3eaa0846d03161d
225061732 <225061732@students.unam.na>
2026-06-09 10:41:57 +0200

    Update index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+// FixFlow - Water Leak Reporting System
+// Developer: Leonard - 225061732
+// Entry point for the application
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
