fea17e11942695ebd2531db9497aa8b90bed40bf
225061732 <225061732@students.unam.na>
2026-06-09 10:35:09 +0200

    feat: add App.js base structure

---
+++ App.js
@@ -0,0 +1,15 @@
+// FixFlow Water Leak Reporting App
+// Developer: Leonard
+// Branch: feature/login-page
+
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>FixFlow - Water Leak Reporting System</h1>
+    </div>
+  );
+}
+
+export default App;
